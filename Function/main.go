dfc3887eb298527f35563c0448eb0e9942c0fcad
Bishop68r <levin.1998.az@mail.ru>
2023-09-23 18:48:17 +0300

    completed

---
+++ Function/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var summa = getSum(5, 5)
+	fmt.Println(summa)
+
+}
+
+func getSum(firstNumber int, secondNumber int) int {
+
+	return firstNumber + secondNumber
+
+}
